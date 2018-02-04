ae5ef1b4979e2c60eff8bd5b9c551fb0c0d3c17a
alejandro aguilar <aaguilar@alejandros-Air.home>
2018-02-04 02:18:53 -0500

    Added splash.fxml, and controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,23 +3,34 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+
+
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		// Start Splash screen first, it will load Main GUI
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("MainGUI.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Splash.fxml"));
+			
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		
+			
+		}	catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/application/splashController.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public  class splashController implements Initializable{
+
+	@FXML
+	private StackPane rootPane; // ID of panel
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		// Generating new Slash Screen thread
+		new SplashScreen().start();
+		
+	}
+
+	
+	// Adding Thread to start loading MainGUI
+	class SplashScreen extends Thread{
+		@Override
+		public void run() {
+				try {
+					Thread.sleep(5000); // let it render 
+					
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							// Loading Main GUI
+						Parent root = null;
+						try {
+							root = FXMLLoader.load(getClass().getResource("MainGUI.fxml"));
+						}	catch (IOException ex){
+							Logger.getLogger(splashController.class.getName()).log(Level.SEVERE, null, ex);	
+						}
+							Scene scene = new Scene(root);
+							Stage stage = new Stage();
+							stage.setScene(scene);
+							stage.show();
+							rootPane.getScene().getWindow().hide(); // remove splash
+						}
+							
+				});
+
+					
+				}	catch (InterruptedException ex){
+					Logger.getLogger(splashController.class.getName()).log(Level.SEVERE, null, ex);	
+				}
+		}
+	}
+}
