f06b0f1ae7046a177790096133e35bf0998b6d8a
alejandro aguilar <aaguilar@nwk-54-220.nwk.ruw.rutgers.edu>
2018-02-27 16:34:19 -0500

    added functions to buttons

---
+++ src/controller/OnlineDocController.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (C) 2018,  @authors
+ * @author Skyler Malinowski  @email skyler.malinowski@gmail.com
+ * @author Arjun Ohri         @email aohri@att.net
+ * @author Alejandro Aguilar  @email alejandro.aguilar1195@gmail.com
+ * @author Raj Balaji         @email nintedraj@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * GNU General Public License v3  
+ * @link https://www.gnu.org/licenses/gpl.html
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+ * Controls online documentation
+ * 
+ * @author Alejandro Aguilar
+ * @version February 2018
+ */
+
+
+
+public class OnlineDocController extends Application {
+
+	private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("RISC V Online Documentation");
+        scene = new Scene(new Browser(),750,500, Color.web("#666970"));
+        stage.setScene(scene);
+        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
+        stage.show();
+    }
+ 
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+class Browser extends Region {
+ 
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+     
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+        // load the web page
+        webEngine.load("https://riscv.org");
+        //add the web view to the scene
+        getChildren().add(browser);
+ 
+    }
+    private Node createSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+ 
+    @Override protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
+    }
+ 
+    @Override protected double computePrefWidth(double height) {
+        return 750;
+    }
+ 
+    @Override protected double computePrefHeight(double width) {
+        return 500;
+    }
+}
