5d59012a34b8ae4e8171fc2451bfb536fbef4982
alejandro aguilar <aaguilar@alejandros-Air.home>
2018-03-15 21:43:59 -0400

    testing

---
+++ src/riscv/InstructionType.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2018,  @authors
+ * @author Skyler Malinowski  @email skyler.malinowski@gmail.com
+ * @author Arjun Ohri         @email aohri@att.net
+ * @author Alejandro Aguilar  @email alejandro.aguilar1195@gmail.com
+ * @author Raj Balaji         @email nintedraj@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * GNU General Public License v3  
+ * @link https://www.gnu.org/licenses/gpl.html
+ */
+
+package riscv;
+
+/**
+ * Valid instruction types to be used by hah map
+ * @author Skyler Malinowski
+ * @version February 2018
+ */
+public enum InstructionType
+{
+	R_Type,
+	I_Type,
+	S_Type,
+	B_Type,
+	U_Type;
+}
